7287d1817854142cc3b1b57af874e31c52860a5a
Darren Coppinger <darrencoppinger@gmail.com>
2020-09-06 23:32:16 +0000

    Product Admin - Editing Products

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # previously path('<product_id>', views.product_detail, name='product_detail'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
