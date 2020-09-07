ba65025ec1150f97bd284d25bbd6c0d42ce8debd
Darren Coppinger <darrencoppinger@gmail.com>
2020-09-07 00:12:20 +0000

    Product Admin - Fixing the image field part 2

--- products/widgets.py
+++ products/widgets.py
@@ -2,8 +2,11 @@ from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
 
 
+# Code from here:
+# https://github.com/django/django/blob/master/django/forms/widgets.py
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('')
+    # change location template is loaded from
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
